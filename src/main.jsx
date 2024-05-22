7fc4b263fde39b98a097fe35e8f94205545297c0
Shuvro0421 <adibarmanshuvro89@gmail.com>
2024-05-22 10:33:15 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Body from './components/Body.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body></Body>
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
